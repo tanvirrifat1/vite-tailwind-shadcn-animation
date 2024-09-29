9cbe77c2fcde853a59d6f6055827d990fe5667cc
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-09-29 13:58:40 +0800

    added

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center my-52">
+      <Button>SomeThing</Button>
+    </div>
+  );
+};
+
+export default App;
